efe62735e2ab0f38f461f50a5815ffeeed5e26f2
Kal <kal.aalrajhi@gmail.com>
2023-07-18 11:31:22 -0400

    Add tab view icons and text

--- Appetizers/AppetizerTabView.swift
+++ Appetizers/AppetizerTabView.swift
@@ -10,7 +10,23 @@ import SwiftUI
 struct AppetizerTabView: View {
     var body: some View {
         TabView {
+            AppetizerListView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
             
+            AccountView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Account")
+                }
+            
+            OrderView()
+                .tabItem {
+                    Image(systemName: "bag")
+                    Text("Order")
+                }
         }
     }
 }
