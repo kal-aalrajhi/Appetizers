be577470c99a9d1d03ecfc8c96436dbb5472d3e3
Kal <kal.aalrajhi@gmail.com>
2023-07-18 11:24:57 -0400

    Create tab view nav files

---
+++ Appetizers/OrderView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderView.swift
+//  Appetizers
+//
+//  Created by Dr Cpt Blackbeard on 7/18/23.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+    }
+}
