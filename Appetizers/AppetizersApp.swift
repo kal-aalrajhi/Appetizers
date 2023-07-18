b04a2d3a154923ee871b577df49406a51d97ddbb
Kal <kal.aalrajhi@gmail.com>
2023-07-18 11:18:56 -0400

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Dr Cpt Blackbeard on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
